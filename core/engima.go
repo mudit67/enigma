26a536560ca5a302533670402d822c5697b1444c
mudit67 <mr.mudit.bhardwaj@gmail.com>
2024-08-25 12:15:50 +0530

    style: remove debugging comments

--- core/engima.go
+++ core/engima.go
@@ -59,29 +59,18 @@ func (en *Enigma) incrementRotors() {
 }
 func (en *Enigma) Cipher(input string) (outStr string) {
 	for _, char := range input {
-		// fmt.Println(input, char, string(char))
 		var out rune
 		out = en.Plugboard.Replace(rune(char))
-		// fmt.Print(string(out), " ")
 		out = en.RotorC.Jumble(out, RightToLeft)
-		// fmt.Print(string(out), " ")
 		out = en.RotorB.Jumble(out, RightToLeft)
-		// fmt.Print(string(out), " ")
 		out = en.RotorA.Jumble(out, RightToLeft)
-		// fmt.Print(string(out), " ")
 		out = en.ReflectorRotor.WiringCfg[out]
-		// fmt.Print(string(out), " ")
 		out = en.RotorA.Jumble(out, LeftToRight)
-		// fmt.Print(string(out), " ")
 		out = en.RotorB.Jumble(out, LeftToRight)
-		// fmt.Print(string(out), " ")
 		out = en.RotorC.Jumble(out, LeftToRight)
-		// fmt.Print(string(out), " ")
 		out = en.Plugboard.Replace(out)
-		// fmt.Println()
 		en.incrementRotors()
 		outStr += string(out)
-		// fmt.Println(outStr, out)
 	}
 	return
 }
